c2f53cfcfe5e6770d057f70aeda559d9d03118c7
Daniel Marinho <contato@danielmarinho.dev>
2023-08-24 18:16:10 -0300

    feat: add json utils

---
+++ utils/jsonUtils.ts
@@ -0,0 +1,8 @@
+/**
+ * @description Parse data into JSON to handle TS conflicts
+ * @param {Object} data
+ * @return JSON
+ */
+export function jsonfy (data: Object): JSON {
+  return JSON.parse(JSON.stringify(data));
+}
