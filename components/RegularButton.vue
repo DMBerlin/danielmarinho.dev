27a3b9c9222bfe95421a92f5aecb926265a1e990
Daniel Marinho <contato@danielmarinho.dev>
2023-08-25 14:39:31 -0300

    refactor: update regular button with lottie icon

--- components/RegularButton.vue
+++ components/RegularButton.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
+import { LottieAnimation } from "lottie-web-vue";
+import { ref } from "vue";
 const props = defineProps<{
-  icon: string;
+  icon: JSON;
   label: string;
   alt?: string;
   callback?: Function;
 }>();
+const lottieRef = ref();
+const animate = () => lottieRef.value.goToAndPlay(0, true);
 </script>
 <template lang="pug">
-button(@click="props.callback").btn-standard
-  img(:src="props.icon" :alt="props.alt")
+button(
+  @mouseenter="animate"
+  @click="props.callback"
+).btn-standard
+  LottieAnimation(
+    :animation-data="props.icon"
+    :auto-play="false"
+    ref="lottieRef"
+    autoplay
+  ).lottie-icon
   span {{ props.label }}
 </template>
 <style scoped lang="scss">
+.lottie-icon {
+  width: 24px;
+  height: 24px;
+  margin-left: 8px;
+}
 .btn-standard {
   display: inline-flex;
   align-items: center;
