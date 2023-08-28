33059d791790a6f9b2127235916ad1072a1f8417
Daniel Marinho <contato@danielmarinho.dev>
2023-08-28 15:53:22 -0300

    update: lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,10 @@ export default defineNuxtConfig({
       },
     },
   },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/device"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/device",
+  ],
   runtimeConfig: {
     public: {
       root: process.env.BASE_URL || "localhost:3000",
