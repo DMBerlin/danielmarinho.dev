fdca116d7245b7c325b2e6c4fd11bbc002112960
Daniel Marinho <contato@danielmarinho.dev>
2023-09-08 13:38:31 -0300

    feat(gradient-title): add customizable size prop

--- components/GradientTitle.vue
+++ components/GradientTitle.vue
@@ -3,13 +3,18 @@ import { computed } from "vue";
 
 const props = defineProps<{
   title: string;
+  size?: string;
   leftColor: string;
   rightColor: string;
 }>();
 
+const fontSize = props.size || '45px';
 const gradientStyle = computed(
   () =>
-    `background-image: linear-gradient(45deg, ${props.leftColor}, ${props.rightColor})`,
+    `
+      background-image: linear-gradient(45deg, ${props.leftColor}, ${props.rightColor});
+      font-size: ${fontSize};
+    `,
 );
 </script>
 <template lang="pug">
@@ -19,7 +24,6 @@ div
 <style scoped lang="scss">
 .gradient-this {
   font-family: $--fonts-heading;
-  font-size: 48px;
   font-weight: bold;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
