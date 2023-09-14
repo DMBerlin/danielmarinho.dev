9ecee966a0983f7bf8f67788a2d0ee54329dfa8d
Daniel Marinho <contato@danielmarinho.dev>
2023-09-14 19:20:37 -0300

    chore(config): update vite params and build

--- types/gradientTitleProps.ts
+++ types/gradientTitleProps.ts
@@ -1,26 +1,28 @@
-type GradientProps = {
-  title: string;
-  size?: string;
+declare module "gradient-title-types" {
+  type GradientProps = {
+    title: string;
+    size?: string;
+  };
+
+  type NeverLeftAndRightColors = {
+    leftColor?: never;
+    rightColor?: never;
+  };
+
+  type NeverCustomGradient = {
+    customGradient?: never;
+  };
+
+  type GradientWithLeftAndRight = {
+    leftColor: string;
+    rightColor: string;
+  } & GradientProps &
+    NeverCustomGradient;
+
+  type GradientWithCustom = {
+    customGradient: string;
+  } & GradientProps &
+    NeverLeftAndRightColors;
+
+  export type GradientTitleProps = GradientWithLeftAndRight | GradientWithCustom;
 };
-
-type NeverLeftAndRightColors = {
-  leftColor?: never;
-  rightColor?: never;
-};
-
-type NeverCustomGradient = {
-  customGradient?: never;
-};
-
-type GradientWithLeftAndRight = {
-  leftColor: string;
-  rightColor: string;
-} & GradientProps &
-  NeverCustomGradient;
-
-type GradientWithCustom = {
-  customGradient: string;
-} & GradientProps &
-  NeverLeftAndRightColors;
-
-export type GradientTitleProps = GradientWithLeftAndRight | GradientWithCustom;
