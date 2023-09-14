cfbcafce95ea9456315e7d2e9d4714620b5dc76b
Daniel Marinho <contato@danielmarinho.dev>
2023-09-14 19:21:54 -0300

    chore(types): create gradient component types

--- types/gradientTitleProps.ts
+++ types/gradientTitleProps.ts
@@ -1,28 +1,26 @@
-declare module "gradient-title-types" {
-  type GradientProps = {
-    title: string;
-    size?: string;
-  };
-
-  type NeverLeftAndRightColors = {
-    leftColor?: never;
-    rightColor?: never;
-  };
-
-  type NeverCustomGradient = {
-    customGradient?: never;
-  };
-
-  type GradientWithLeftAndRight = {
-    leftColor: string;
-    rightColor: string;
-  } & GradientProps &
-    NeverCustomGradient;
-
-  type GradientWithCustom = {
-    customGradient: string;
-  } & GradientProps &
-    NeverLeftAndRightColors;
-
-  export type GradientTitleProps = GradientWithLeftAndRight | GradientWithCustom;
+type GradientProps = {
+  title: string;
+  size?: string;
 };
+
+type NeverLeftAndRightColors = {
+  leftColor?: never;
+  rightColor?: never;
+};
+
+type NeverCustomGradient = {
+  customGradient?: never;
+};
+
+type GradientWithLeftAndRight = {
+  leftColor: string;
+  rightColor: string;
+} & GradientProps &
+  NeverCustomGradient;
+
+type GradientWithCustom = {
+  customGradient: string;
+} & GradientProps &
+  NeverLeftAndRightColors;
+
+export type GradientTitleProps = GradientWithLeftAndRight | GradientWithCustom;
