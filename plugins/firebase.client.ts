a08b4623d3c08eb96264e8b0f4faedc933f3d881
Daniel Marinho <contato@danielmarinho.dev>
2024-02-20 22:14:43 -0300

    feat(1.1.0): create firebase plugin

---
+++ plugins/firebase.client.ts
@@ -0,0 +1,30 @@
+import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { defineNuxtPlugin, useRuntimeConfig } from "#app";
+import { RuntimeConfig } from "nuxt/schema";
+
+export default defineNuxtPlugin({
+  name: "firebase",
+  setup(nuxtApp) {
+    const config: RuntimeConfig = useRuntimeConfig();
+
+    const app: FirebaseApp = initializeApp({
+      apiKey: config.public.firebaseApiKey,
+      authDomain: config.public.firebaseAuthDomain,
+      projectId: config.public.firebaseProjectId,
+      storageBucket: config.public.firebaseStorageBucket,
+      messagingSenderId: config.public.firebaseMessagingSenderId,
+      appId: config.public.firebaseAppId,
+      measurementId: config.public.firebaseMeasurementId,
+    } as FirebaseOptions);
+    const analytics: Analytics = getAnalytics(app);
+    const firestore: Firestore = getFirestore(app);
+
+    nuxtApp.vueApp.provide("firestore", firestore);
+    nuxtApp.provide("firestore", firestore);
+
+    nuxtApp.vueApp.provide("analytics", analytics);
+    nuxtApp.provide("analytics", analytics);
+  },
+});
