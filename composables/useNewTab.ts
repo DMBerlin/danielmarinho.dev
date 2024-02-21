02c3f190254fa6e1578566749e1cd42a03dc8c46
Daniel Marinho <contato@danielmarinho.dev>
2024-02-20 22:15:30 -0300

    refactor(1.1.0): add logging to useNewTab

--- composables/useNewTab.ts
+++ composables/useNewTab.ts
@@ -1,2 +1,7 @@
-export const useNewTab = async (url: string): Promise<Window> =>
+import { getAnalytics, logEvent } from "@firebase/analytics";
+import { EventName } from "~/types/useLogEvent";
+
+export const useNewTab = async (url: string): Promise<void> => {
+  logEvent(getAnalytics(), EventName.OPEN_URL, { value: { url } });
   await window.open(url, "_blank");
+};
