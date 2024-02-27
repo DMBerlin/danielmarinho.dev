da4e6c8fc8f682f55c045cd7539c14591abc24d1
Daniel Marinho <contato@danielmarinho.dev>
2024-02-26 21:30:25 -0300

    feat(remote-config): add config on plugins

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -3,6 +3,7 @@ import { Firestore, getFirestore } from "firebase/firestore";
 import { Analytics, getAnalytics } from "firebase/analytics";
 import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 import { RuntimeConfig } from "nuxt/schema";
+import { getRemoteConfig, RemoteConfig } from "firebase/remote-config";
 
 export default defineNuxtPlugin({
   name: "firebase",
@@ -18,13 +19,18 @@ export default defineNuxtPlugin({
       appId: config.public.firebaseAppId,
       measurementId: config.public.firebaseMeasurementId,
     } as FirebaseOptions);
+
     const analytics: Analytics = getAnalytics(app);
     const firestore: Firestore = getFirestore(app);
+    const remoteConfig: RemoteConfig = getRemoteConfig(app);
 
     nuxtApp.vueApp.provide("firestore", firestore);
     nuxtApp.provide("firestore", firestore);
 
     nuxtApp.vueApp.provide("analytics", analytics);
     nuxtApp.provide("analytics", analytics);
+
+    nuxtApp.vueApp.provide("remoteConfig", remoteConfig);
+    nuxtApp.provide("remoteConfig", remoteConfig);
   },
 });
