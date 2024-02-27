6c7b55359f180b6127b1497f0aa0cefd36466af2
Daniel Marinho <contato@danielmarinho.dev>
2024-02-26 21:32:26 -0300

    feat(resend): dispatch email on resume download

--- server/api/files.ts
+++ server/api/files.ts
@@ -1,4 +1,13 @@
 import { defineEventHandler } from "h3";
 import { FirebaseService } from "~/server/services/firebase.service";
+import { MailService } from "~/server/services/mail.service";
 
-export default defineEventHandler(() => FirebaseService.getResumeFilepath());
+export default defineEventHandler(() =>
+  FirebaseService.getResumeFilepath().then(async (filepath: string) => {
+    await MailService.sendDownloadNotification(
+      "Resumé downloaded!",
+      `<p>Congrats! Someone just downloaded your resumé at <strong>${new Date().toLocaleDateString()}</strong>!</p>.`,
+    );
+    return filepath;
+  }),
+);
