4462ad0b22cabd2fe0b06fb1b460a8da79331089
Daniel Marinho <contato@danielmarinho.dev>
2024-03-02 12:49:34 -0300

    feat: use env type

--- composables/useLogEvent.ts
+++ composables/useLogEvent.ts
@@ -1,15 +1,15 @@
 import { logEvent, getAnalytics } from "@firebase/analytics";
 import { LogEvent, UseLogEvent } from "~/types/useLogEvent";
+import { AppEnvironment } from "~/types/appEnvironment";
 
 export const useLogEvent = (): UseLogEvent => ({
   emit: (eventName: LogEvent, eventParams?: unknown): void => {
-    if (process.env.NODE_ENV === "production") {
-      logEvent(
-        getAnalytics(),
-        eventName,
-        { value: eventParams },
-        { global: true },
-      );
-    }
+    if (process.env.NODE_ENV === AppEnvironment.DEV) return;
+    logEvent(
+      getAnalytics(),
+      eventName,
+      { value: eventParams },
+      { global: true },
+    );
   },
 });
