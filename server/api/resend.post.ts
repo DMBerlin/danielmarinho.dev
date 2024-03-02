4e10ae122df3fe81469a4a229b7a844c2d383d69
Daniel Marinho <contato@danielmarinho.dev>
2024-03-02 12:49:07 -0300

    style(lint): update

--- server/api/resend.post.ts
+++ server/api/resend.post.ts
@@ -4,7 +4,8 @@ import { MailService } from "~/server/services/mail.service";
 export default defineEventHandler(
   async (event: H3Event<EventHandlerRequest>) => {
     const body = await readBody(event);
-    if (!body.subject || !body.message) return { statusCode: 404, status: "BAD_REQUEST" };
+    if (!body.subject || !body.message)
+      return { statusCode: 404, status: "BAD_REQUEST" };
     await MailService.sendDownloadNotification(body.subject, body.message);
     return { statusCode: 200, status: "OK" };
   },
